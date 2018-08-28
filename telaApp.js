479242353a3fd7b2b32872d50f6bc0c2cf99ed3f
williamantoniozup <william.antonio@zup.com.br>
2018-08-28 14:55:22 -0300

    First comment

---
+++ telaApp.js
@@ -0,0 +1,22 @@
+/*William Henrique*/
+
+/*
+var count = 1;
+function setColor(btn, color) {
+    var property = document.getElementById(btn);
+    if (count == 0) {
+        property.style.backgroundColor = "#FFFFFF"
+        count = 1;
+    }
+    else {
+        property.style.backgroundColor = "#7FFF00"
+        count = 0;
+    }
+}
+*/
+
+var btnCheckPerfil = document.querySelector('btnCheckPerfil');
+
+btnCheckPerfil.onclick = function(){
+
+}
