dfc49e989fa861b41b1ea7ca148049a63048a7ea
williamantoniozup <william.antonio@zup.com.br>
2018-08-29 15:03:18 -0300

    inicio do httpRequest

--- telaApp.js
+++ telaApp.js
@@ -1,22 +1,13 @@
-/*William Henrique*/
-
 /*
-var count = 1;
-function setColor(btn, color) {
-    var property = document.getElementById(btn);
-    if (count == 0) {
-        property.style.backgroundColor = "#FFFFFF"
-        count = 1;
-    }
-    else {
-        property.style.backgroundColor = "#7FFF00"
-        count = 0;
-    }
-}
-document.querySelector('td.columnImgPerfil').onclick = function(){
-    alert('teste');
-}
+William Henrique
+
+
 */
+
+var xmlhttp = new XMLHttpRequest();
+var url = "https://randomuser.me/api/?results=10";
+
+
 
 
 var headerNav = document.getElementById("divNav");
@@ -26,8 +17,7 @@ for (var i=0; i<btnsNav.length; i++){
         var current = document.getElementsByClassName("activeNav");
         current[0].className = current[0].className.replace(" activeNav", "");
         this.className += " activeNav";
-    });
-    
+    }); 
 }
 
 
