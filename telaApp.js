2a1c61b1e51d4eee93fb8b0e53a8b1d4d465c3bf
williamantoniozup <william.antonio@zup.com.br>
2018-08-29 14:42:33 -0300

    menu nav fazendo transição

--- telaApp.js
+++ telaApp.js
@@ -19,6 +19,20 @@ document.querySelector('td.columnImgPerfil').onclick = function(){
 */
 
 
+var headerNav = document.getElementById("divNav");
+var btnsNav = headerNav.getElementsByClassName("divItem");
+for (var i=0; i<btnsNav.length; i++){
+    btnsNav[i].addEventListener("click",function(){
+        var current = document.getElementsByClassName("activeNav");
+        current[0].className = current[0].className.replace(" activeNav", "");
+        this.className += " activeNav";
+    });
+    
+}
+
+
+
+
 
 
 
